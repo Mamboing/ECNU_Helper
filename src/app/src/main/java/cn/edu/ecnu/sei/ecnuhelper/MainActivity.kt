55eb301558b5c2754dec53ed0fd24fae83449412
CCXXXI <ccxxxi@qq.com>
2021-04-15 11:17:17 +0800

    init empty activity

---
+++ src/app/src/main/java/cn/edu/ecnu/sei/ecnuhelper/MainActivity.kt
@@ -0,0 +1,11 @@
+package cn.edu.ecnu.sei.ecnuhelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
