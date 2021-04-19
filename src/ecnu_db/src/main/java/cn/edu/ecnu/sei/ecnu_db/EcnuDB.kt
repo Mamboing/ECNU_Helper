61ea140caf13ae4c5914fabbe01461b1efdab2a9
CCXXXI <ccxxxi@qq.com>
2021-04-19 15:12:20 +0800

    reformat

--- src/ecnu_db/src/main/java/cn/edu/ecnu/sei/ecnu_db/EcnuDB.kt
+++ src/ecnu_db/src/main/java/cn/edu/ecnu/sei/ecnu_db/EcnuDB.kt
@@ -1,19 +1,21 @@
 package cn.edu.ecnu.sei.ecnu_db
 
-@Suppress("unused")
-object EcnuDB {
-    @Suppress("SpellCheckingInspection")
-    const val PORTAL_URL = "https://portal1.ecnu.edu.cn/cas/login" +
-            "?service=http%3A%2F%2Fapplicationnewjw.ecnu.edu.cn%2Feams%2Fhome.action"
-    const val TABLE_URL = "http://applicationnewjw.ecnu.edu.cn/eams/" +
-            "courseTableForStd!courseTable.action"
-    const val IDS_URL = "http://applicationnewjw.ecnu.edu.cn/eams/" +
-            "courseTableForStd!index.action"
-    const val CAPTCHA_URL = "https://portal1.ecnu.edu.cn/cas/code"
+@Suppress("SpellCheckingInspection")
+private object URL {
+    private const val CAS = "portal1.ecnu.edu.cn/cas"
+    private const val JW = "applicationnewjw.ecnu.edu.cn"
 
-    @Suppress("SpellCheckingInspection")
+    const val PORTAL = "https://$CAS/login?service=http%3A%2F%2F$JW%2Feams%2Fhome.action"
+    const val TABLE = "http://$JW/eams/courseTableForStd!courseTable.action"
+    const val IDS = "http://$JW/eams/courseTableForStd!index.action"
+    const val CAPTCHA = "https://$CAS/code"
+
     val HEADERS = mapOf(
         "User-Agent" to "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
-        "Refer" to "https://portal1.ecnu.edu.cn/cas/login?service=http%3A%2F%2Fapplicationnewjw.ecnu.edu.cn%2Feams%2Fhome.action%3Bjs"
+        "Refer" to "$PORTAL%3Bjs"
     )
+}
+
+object EcnuDB {
+
 }
